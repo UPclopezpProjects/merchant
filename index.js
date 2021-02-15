912ddb535d9bcac9a4b848ac4a4f5e917d710e83
Oznnniv <56807527+Oznnniv@users.noreply.github.com>
2021-02-15 16:06:17 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+'user strict'
+
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3002;
+var mongoDB = 'mongodb://172.17.0.1:27017/merchants';
+
+
+mongoose.connect(mongoDB, {useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true}, (err, res) => {
+	if(err){
+		throw err;
+	}else{
+		console.log("Conexión exitosa (Base de datos)...");
+		app.listen(port, function(){
+			console.log("Microservicio 'Merchant' escuchando en -> http://localhost:"+port);
+		});
+	}
+});
